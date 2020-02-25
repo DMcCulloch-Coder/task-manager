b8c8645bbaa1cf32a62522bf47c2ab2bbf80eef5
David <DMcCulloch86@gmail.com>
2020-02-24 20:30:30 -0500

    working on css for buttons and setting up variables

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <div>
+    <div className="header">
         <h1>Task Manager</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
         <NavLink to="/create" activeClassName="is-active" exact={true}>Add Task</NavLink>
