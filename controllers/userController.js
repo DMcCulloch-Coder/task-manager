45fa49b5ffa4520b242c506d12dfd4bd4bc80c0e
David <DMcCulloch86@gmail.com>
2020-03-01 17:06:41 -0500

    fixed bug that didn't allow me to update users, also added controller to handle getting users

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,9 +13,19 @@ module.exports = {
     },
 
     findById: async (req, res) => {
-        const user = User.findById(req.params.id)
 
-        res.send(user)
+        try {
+            const user = await User.findById(req.params.id)
+
+            if (!user) {
+                return res.status(404).send();
+            }
+
+            res.send(user)
+        } catch (e) {
+            res.status(500).send()
+        }
+
     },
 
     login: async (req, res) => {
@@ -26,21 +36,17 @@ module.exports = {
         const updates = Object.keys(req.body)
         const allowedUpdates = ['name', 'email', 'password']
         const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-        console.log('working 1')
 
         if (!isValidOperation) {
             return res.status(400).send({ 'error': 'Invalid Update' })
         }
 
         try {
-            console.log('working 2')
-            const user = await User.findbyId(req.params.id);
+            const user = await User.findById(req.params.id)
 
             updates.forEach((update) => user[update] = req.body[update]);
 
             await user.save();
-
-            console.log('working 3');
 
             if (!user) {
                 return res.status(404).send({ 'error': "" })
