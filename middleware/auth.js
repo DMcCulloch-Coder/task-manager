7887c0203c9b50fafca11d34e46b554687d4aed4
David <DMcCulloch86@gmail.com>
2020-03-07 13:08:58 -0500

    adding logout routes and env file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+require('dotenv').config();
 
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
-        const decoded = jwt.verify(token, 'tempsecreat');
+        const decoded = jwt.verify(token, process.env.ENV_secret);
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
         if (!user) {
