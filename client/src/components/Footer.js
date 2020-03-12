1746c0a26aeaf473daa3daeaaa7097a63ab56e99
David <DMcCulloch86@gmail.com>
2020-03-11 21:36:20 -0400

    added github link to webpage and continue to work on css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,11 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 const Footer = () => (
     <div className="footer">
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active" exact={true}>Add Task</NavLink>
-        <NavLink to="/profile" activeClassName="is-active" exact={true}>Profile</NavLink>
-        <NavLink to="/signup" activeClassName="is-active" exact={true}>Sign Up</NavLink>
-        <NavLink to="/login" activeClassName="is-active" exact={true}>Login</NavLink>
+        <div className="footer__wrapper">
+            <NavLink className="footer__links" to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+            <NavLink className="footer__links" to="/create" activeClassName="is-active" exact={true}>Add Task</NavLink>
+            <NavLink className="footer__links" to="/profile" activeClassName="is-active" exact={true}>Profile</NavLink>
+            <NavLink className="footer__links" to="/signup" activeClassName="is-active" exact={true}>Sign Up</NavLink>
+            <NavLink className="footer__links" to="/login" activeClassName="is-active" exact={true}>Login</NavLink>
+            <a href="https://github.com/DMcCulloch-Coder" className="footer__credits" target="_blank" alt="Link to David's Github">Created By: David McCulloch</a>
+        </div>
     </div>
 
 )
